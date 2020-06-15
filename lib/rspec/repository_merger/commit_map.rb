d6df24c1492c6d5a74e1ee5bbeaff32a60120373
Yuji Nakayama <nkymyj@gmail.com>
2020-06-16 01:37:46 +0900

    Persist commit map as a JSON file

--- lib/rspec/repository_merger/commit_map.rb
+++ lib/rspec/repository_merger/commit_map.rb
@@ -1,18 +1,30 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module RSpec
   class RepositoryMerger
     class CommitMap
+      def self.load_from(path)
+        json = File.read(path)
+        new(JSON.parse(json))
+      end
+
+      def initialize(map = {})
+        @map = map
+      end
+
+      def save_to(path)
+        json = JSON.pretty_generate(@map)
+        File.write(path, json)
+      end
+
       def register(commit_id_in_merged_repo:, original_commit:)
-        map[original_commit_key(original_commit)] = commit_id_in_merged_repo
+        @map[original_commit_key(original_commit)] = commit_id_in_merged_repo
       end
 
       def commit_id_in_merged_repo_for(original_commit)
-        map[original_commit_key(original_commit)]
-      end
-
-      def map
-        @map ||= {}
+        @map[original_commit_key(original_commit)]
       end
 
       def original_commit_key(commit)
