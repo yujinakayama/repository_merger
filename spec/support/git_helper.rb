eb5fcb38e22d58506408aba285e0bed6692932f6
Yuji Nakayama <nkymyj@gmail.com>
2021-08-30 00:29:27 +0900

    Improve GitError message in specs

--- spec/support/git_helper.rb
+++ spec/support/git_helper.rb
@@ -3,7 +3,11 @@ require 'open3'
 require 'shellwords'
 
 module GitHelper
-  GitError = Class.new(StandardError)
+  class GitError < StandardError
+    def initialize(command, stderr)
+      super("#{stderr.chomp} (#{command.shelljoin} in #{Dir.getwd})")
+    end
+  end
 
   module_function
 
@@ -85,8 +89,9 @@ module GitHelper
 
   def git(arg)
     args = arg.is_a?(Array) ? arg : arg.shellsplit
-    stdout, stderr, status = Open3.capture3('git', *args)
-    raise GitError, stderr unless status.success?
+    command = ['git'] + args
+    stdout, stderr, status = Open3.capture3(*command)
+    raise GitError.new(command, stderr) unless status.success?
     stdout.each_line.map { |line| line.sub(/ +$/, '') }.join
   end
 
